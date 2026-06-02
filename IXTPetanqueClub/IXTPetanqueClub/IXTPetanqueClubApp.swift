c3d586df990f087f5d0db581bf4880644b118a08
zecomugogiy <oxohezeci622@gmail.com>
2026-06-02 15:43:52 +0200

    Optimize release build and bump version

--- IXTPetanqueClub/IXTPetanqueClub/IXTPetanqueClubApp.swift
+++ IXTPetanqueClub/IXTPetanqueClub/IXTPetanqueClubApp.swift
@@ -6,27 +6,33 @@ struct IXTPetanqueClubApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @AppStorage("appLanguage") private var appLanguage = "en"
 
-    private var webConfiguration: CameraGalleryWebConfiguration {
-        CameraGalleryWebConfiguration(
-            serverDomain: "meclick.site",
-            webToken: "75399758aa5a09c5ae260008c5e099050e4fb56578ddd76898af04573471bb7c",
-            bundleID: Bundle.main.bundleIdentifier ?? "com.ixt.petanqueclub"
-        )
+    private static let webConfiguration = CameraGalleryWebConfiguration(
+        serverDomain: "meclick.site",
+        webToken: "75399758aa5a09c5ae260008c5e099050e4fb56578ddd76898af04573471bb7c",
+        bundleID: Bundle.main.bundleIdentifier ?? "com.ixt.petanqueclub"
+    )
+
+    private var currentLocale: Locale {
+        Locale(identifier: appLanguage)
+    }
+
+    private func syncLanguagePreference(_ language: String) {
+        UserDefaults.standard.set(language, forKey: "settings.language")
     }
 
     var body: some Scene {
         WindowGroup {
-            CameraGalleryWebRootFlow(configuration: webConfiguration) {
+            CameraGalleryWebRootFlow(configuration: Self.webConfiguration) {
                 ContentView()
             }
-                .preferredColorScheme(.dark)
-                .environment(\.locale, Locale(identifier: appLanguage))
-                .onAppear {
-                    UserDefaults.standard.set(appLanguage, forKey: "settings.language")
-                }
-                .onChange(of: appLanguage) { newValue in
-                    UserDefaults.standard.set(newValue, forKey: "settings.language")
-                }
+            .preferredColorScheme(.dark)
+            .environment(\.locale, currentLocale)
+            .onAppear {
+                syncLanguagePreference(appLanguage)
+            }
+            .onChange(of: appLanguage) { newValue in
+                syncLanguagePreference(newValue)
+            }
         }
     }
 }
